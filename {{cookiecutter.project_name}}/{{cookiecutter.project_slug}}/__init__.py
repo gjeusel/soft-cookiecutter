9cf705eb6f4a7de355cecaaf0dfd179f5c52744f
Jeusel Guillaume <guillaume.jeusel@renewex.co>
2021-07-03 21:58:10 +0200

    [FIX] dynamic versioning with git

--- {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/__init__.py
@@ -6,5 +6,9 @@ try:
 except Exception:
     __version__ = 'Version not found.'
 {% else -%}
-__version__ = "0.0.0"
+import dunamai as _dunamai
+
+__version__ = _dunamai.get_version(
+    __name__, first_choice=_dunamai.Version.from_any_vcs
+).serialize(style=_dunamai.Style.Pep440, bump=True)
 {%- endif %}
